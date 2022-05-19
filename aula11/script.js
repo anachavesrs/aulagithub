2fec66f0c3dd4f6c0427450ce7d0ebe0a68b4fbe
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-05-19 20:02:08 -0300

    commit

---
+++ aula11/script.js
@@ -0,0 +1,21 @@
+const objs = [
+    {
+        "Nome" : "Joaquim",
+        "Idade" : "7",
+        "Email": "joaquim@gmail.com",
+        "statusatividadeextra" : true,
+        "atvextra" : ["natação", "judo"]
+
+    },
+
+    {
+        "Nome" : "Juliana",
+        "Idade" : "8",
+        "Email": "julia@gmail.com",
+        "statusatividadeextra" : false,
+        "atvextra" : [null]
+
+    }
+]
+console.log (objs);
+console.log(typeof objs);
